9a94ae1488c0ffb3ec89f53f4de7c40c36f9758c
kaustubh3000 <kaustubhsingh519@gmail.com>
2020-11-25 23:39:16 +0530

    navbutton issue not solved yet

--- js/script.js
+++ js/script.js
@@ -1,46 +1,27 @@
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
-
-    // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-    $(".navbar-toggler").blur(function (event) {
-        var screenWidth = window.innerWidth;
-        if (screenWidth < 992) {
-            $(".navbar-collapse").collapse('hide');
-            $('#navbar-button').children('span').removeClass('fa fa-times fa-lg');
-            $('#navbar-button').children('span').addClass('navbar-toggler-icon');
-        }
-    });
-
-    // In Firefox and Safari, the click event doesn't retain the focus
-    // on the clicked button. Therefore, the blur event will not fire on
-    // user clicking somewhere else in the page and the blur event handler
-    // which is set up above will not be called.
-    // Solution: force focus on the element that the click event fired on
-    $(".navbar-toggler").click(function (event) {
-        $(event.target).focus();
-    });
-});
 
 
-$(document).ready(function (){
-    $('#navbar-button').click(function (){
-        if ($('#navbar-button').children('span').hasClass('navbar-toggler-icon')){
+$(document).ready(function () {
+    //change navbar icon on click
+    $('#navbar-button').click(function () {
+        if ($('#navbar-button').children('span').hasClass('navbar-toggler-icon')) {
             $('#navbar-button').children('span').removeClass('navbar-toggler-icon');
             $('#navbar-button').children('span').addClass('fa fa-times fa-lg');
         }
-        else if ($('#navbar-button').children('span').hasClass('fa fa-times fa-lg')){
+        else if ($('#navbar-button').children('span').hasClass('fa fa-times fa-lg')) {
             $('#navbar-button').children('span').removeClass('fa fa-times fa-lg');
             $('#navbar-button').children('span').addClass('navbar-toggler-icon');
         }
     });
 
+
+    // Collapse the navbar by clicking on the screen except Navbar
+    // $('body').not(document.getElementById(".navbar")).click(function() {
+    //     $(".navbar-collapse").collapse('hide');
+    //     $('#navbar-button').children('span').removeClass('fa fa-times fa-lg');
+    //     $('#navbar-button').children('span').addClass('navbar-toggler-icon');
+    // });
+
+
+    // Collapse the Navbar on scrolling
 });
 
-
-// $(window).scroll(function () {
-//     var screenWidth = window.innerWidth;
-//     if (screenWidth < 992) {
-//         $(".navbar-collapse").collapse('hide');
-//         $('#navbar-button').children('span').removeClass('fa fa-times fa-lg');
-//         $('#navbar-button').children('span').addClass('navbar-toggler-icon');
-//     }
-// });
