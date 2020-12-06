37cd63a02c66def8f24eb337bda2afe81d9add7a
Kaustubh Singh <kaustubh5410@gmail.com>
2020-12-07 00:45:36 +0530

    major changes in footer --------------------------------------- added Links, Contact and References Changes in Developer Contact

--- js/script.js
+++ js/script.js
@@ -23,5 +23,15 @@ $(document).ready(function () {
 
 
     // Collapse the Navbar on scrolling
+
+
+
 });
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 
